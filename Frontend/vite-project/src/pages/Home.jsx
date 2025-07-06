551efc22f0fe8b7ba66fc37449a57a6755264e8b
Kharat-Vaishali <kharatvaishali786@gmail.com>
2025-07-06 07:39:26 -1200

    Job -Finding-Portal

---
+++ Frontend/vite-project/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import TopNiches from '../components/TopNiches'
+import HowltWorks  from '../components/HowltWorks'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Hero></Hero>
+      <TopNiches></TopNiches>
+      <HowltWorks></HowltWorks>
+    </div>
+  )
+}
+
+export default Home
